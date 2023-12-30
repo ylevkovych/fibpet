647e87c92cb38feb1413c51dd8c9cfb89ca8ab35
Yurii Levkovych <ylevkovych@gmail.com>
2023-12-30 22:00:25 +0100

    added nginx in front of app

--- src/main/java/com/levkip/fibpet/ConfigRedis.java
+++ src/main/java/com/levkip/fibpet/ConfigRedis.java
@@ -1,9 +1,7 @@
 package com.levkip.fibpet;
 
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
